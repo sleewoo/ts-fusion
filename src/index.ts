0c63d19061b5ac956b1dcba59d7de38e468b9d2e
Slee Woo <mail@sleewoo.com>
2025-04-24 21:18:02 +0300

    fix(mappedTypeHandler): readonly and plus/minus modifiers

--- src/index.ts
+++ src/index.ts
@@ -379,8 +379,50 @@ const handlerStack: HandlerStack = {
 
           const nameTypeNode = typeNode.getNameTypeNode();
 
+          const readonlyToken = typeNode.getReadonlyToken();
+          const questionToken = typeNode.getQuestionToken();
+
+          let preModifiers = "";
+          let postModifiers = "";
+
+          if (readonlyToken) {
+            /**
+             * readonlyToken.getText() returns "readonly" when no plus/minus token preceed.
+             * and returns only the plus/minus token if it exists.
+             * eg:
+             * for `{ readonly [...] }` it returns "readonly"
+             * for `{ +readonly [...] }` it returns "+"
+             * for `{ -readonly [...] }` it returns "-"
+             *
+             * */
+            preModifiers = format(
+              "%sreadonly ",
+              readonlyToken.getText().replace("readonly", ""),
+            );
+          }
+
+          if (questionToken) {
+            /**
+             * extracting 3rd sibling (index 2) from mappedTypeNode,
+             * it would be plus/minus token, if provided.
+             * eg:
+             * for `{ [...]?:  mappedTypeNode }` 3rd sibling would be "]"
+             * for `{ [...]-?: mappedTypeNode }` 3rd sibling would be "-"
+             * for `{ [...]+?: mappedTypeNode }` 3rd sibling would be "+"
+             * */
+            const thirdSibling = mappedTypeNode?.getPreviousSiblings()[2];
+            if (thirdSibling?.isKind(SyntaxKind.MinusToken)) {
+              postModifiers = "-?";
+            } else if (thirdSibling?.isKind(SyntaxKind.PlusToken)) {
+              postModifiers = "+?";
+            } else {
+              postModifiers = "?";
+            }
+          }
+
           return format(
-            "{ [%s in %s%s]%s: %s }",
+            "{ %s[%s in %s%s]%s: %s }",
+            preModifiers,
             mappedTypeParameter.getNameNode().getText(),
             mappedTypeConstraint
               ? next({
@@ -399,7 +441,7 @@ const handlerStack: HandlerStack = {
                   }),
                 )
               : "",
-            typeNode.getQuestionToken() ? "?" : "",
+            postModifiers,
             mappedTypeNode
               ? next({
                   typeNode: mappedTypeNode,
